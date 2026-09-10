d273e7f5da58a84a6ff7afc5746eaad397dd94a3
Rafi Pangestu <rafipangestu0875@gmail.com>
2026-09-10 11:39:23 +0800

    feat(m9.0): add legacy root migration baseline

--- backend/alembic/versions/b1f9dbe772fa_m1_initial_schema.py
+++ backend/alembic/versions/b1f9dbe772fa_m1_initial_schema.py
@@ -13,7 +13,7 @@ from sqlalchemy.dialects import postgresql
 
 # revision identifiers, used by Alembic.
 revision: str = 'b1f9dbe772fa'
-down_revision: Union[str, None] = None
+down_revision: Union[str, None] = '8e973e84a9d7'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
